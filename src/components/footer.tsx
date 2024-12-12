1f84ea39a553f51d42457b951cd2db930e6bf9d5
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-12-12 22:50:52 +0100

    Enhance Footer component with social links and improved styling
    - Updated Footer component to include social media icons for GitHub, LinkedIn, and email.
    - Improved layout and styling with Tailwind CSS for better visual appeal.
    - Added a descriptive text about the technologies used in the project.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,37 @@
+import { GithubIcon, LinkedinIcon, MailIcon } from 'lucide-react';
+
 export default function Footer() {
-  return <footer className="container mx-auto p-4 border-t">Footer</footer>;
+  return (
+    <footer className="p-6 border-t bg-muted">
+      <div className="max-w-3xl mx-auto">
+        <div className="flex justify-center space-x-6 mb-4">
+          <a
+            href="https://github.com/bricard-dev"
+            className="text-primary hover:text-muted-foreground"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon className="w-4 h-4" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bastienricard/"
+            className="text-primary hover:text-muted-foreground"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinIcon className="w-4 h-4" />
+          </a>
+          <a
+            href="mailto:bastien.ricard-pro@outlook.com"
+            className="text-primary hover:text-muted-foreground"
+          >
+            <MailIcon className="w-4 h-4" />
+          </a>
+        </div>
+        <div className="text-center text-sm text-muted-foreground">
+          <p>Developped with Next.js, Shadcn/UI and Tailwind CSS.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
